22a1e5a0029a29712f624511d15ab9d96c709c53
numata <nununu.mono@gmail.com>
2018-03-18 20:44:00 +0900

    vote, result機能追加

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -20,9 +20,9 @@ def detail(request, question_id):
     return render(request, 'polls/details.html', {'question': question})
 
 
-def result(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
@@ -38,4 +38,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponse(reverse('polls/results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
