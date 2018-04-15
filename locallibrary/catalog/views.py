9c0c9c9c96055268f3676b1e92c902106938295e
numata <nununu.mono@gmail.com>
2018-04-15 15:37:09 +0900

    特定のキーワードを含む本の数を追加

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -17,6 +17,9 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()  # all() is implied by default.
 
+    num_books_miss = Book.objects.filter(title__contains='miss').count()
+    num_genre_miss = Genre.objects.filter(name__contains='miss').count()
+
     # Render the HTML template index. html with the data in the context variable
     return render(
         request,
@@ -26,5 +29,7 @@ def index(request):
             'num_instances': num_instances,
             'num_instances_available': num_instances_available,
             'num_authors': num_authors,
+            'num_books_miss': num_books_miss,
+            'num_genre_miss': num_genre_miss,
         }
     )
