4a5d145eec6a02358b8064f7aba1737f4338aca5
numata <nununu.mono@gmail.com>
2018-04-16 19:48:07 +0900

    authorのリスト-詳細ページ追加

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
 
     path('books/', views.BookListView.as_view(), name='books'),
 
-    path('books/<int:pk>', views.BookDetailView.as_view(), name='book-detail')
+    path('books/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+
+    path('author/', views.AuthorListView.as_view(), name='author'),
+
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
