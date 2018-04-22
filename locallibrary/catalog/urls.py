2b04a6be00ac0b6198fa52c5f09fc16858893791
numata <nununu.mono@gmail.com>
2018-04-22 13:41:29 +0900

    全貸し出しリストを権限付で追加

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+
+    path('loanedbooks/', views.LoanedAllBooksListView.as_view(), name='all-borrowed'),
 ]
