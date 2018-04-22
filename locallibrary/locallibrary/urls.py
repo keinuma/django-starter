961fedeb0e6fbc355b188d79a981ab23ed918a9f
numata <nununu.mono@gmail.com>
2018-04-22 12:36:11 +0900

    認証機能の追加

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -22,6 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    path('accounts/', include('django.contrib.auth.urls')),
+
     path('catalog/', include('catalog.urls')),
 
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
