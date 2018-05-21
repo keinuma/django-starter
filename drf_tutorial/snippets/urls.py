55079d2d7986f3a2255519d3bffd06d71ce1d4ff
numata <nununu.mono@gmail.com>
2018-05-21 20:30:42 +0900

    snippetに認証処理追加、クリエイターをリレーション、パーミッションのカスタム

--- drf_tutorial/snippets/urls.py
+++ drf_tutorial/snippets/urls.py
@@ -7,6 +7,9 @@ from . import views
 urlpatterns = [
     path('snippets/', views.SnippetListGeneric.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetailGeneric.as_view()),
+
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>', views.UserDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- drf_tutorial/snippets/views.py
+++ drf_tutorial/snippets/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics
+from rest_framework import permissions
 
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .serializers import SnippetSerializer, UserSerializer
+from .permissions import IsOwnerOrReadOnly
 
 
 @api_view(['GET', 'POST'])
@@ -95,8 +98,24 @@ class SnippetDetail(APIView):
 class SnippetListGeneric(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetailGeneric(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
