d1544be26b4a3a19c294a5afe50aadb171d40ecf
numata <nununu.mono@gmail.com>
2018-05-23 00:12:16 +0900

    フロントエンドとバックエンドを連携

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,28 @@
 <template>
   <v-app id="app">
-    <div>
-      <v-btn>マテリアルボタン</v-btn>
-      <img src="./assets/logo.png">
-      <router-view/>
-    </div>
+    <v-navigation-drawer app></v-navigation-drawer>
+    <v-toolbar app></v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App'
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
