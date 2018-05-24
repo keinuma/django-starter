a89fcc4bf156544bf76fc71ac03aaf7bd2923f15
numata <nununu.mono@gmail.com>
2018-05-24 15:11:37 +0900

    serializer追加

--- tutorial/polls/serializers.py
+++ tutorial/polls/serializers.py
@@ -3,13 +3,17 @@ from rest_framework import serializers
 from .models import Question, Choice
 
 
-class QuestionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Question
-        fields = ('id', 'question_text', 'pub_date')
-
-
 class ChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Choice
         fields = ('id', 'question', 'choice_text', 'votes')
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    choices = ChoiceSerializer(
+        many=True, source='choice_setZZ',
+    )
+
+    class Meta:
+        model = Question
+        fields = ('id', 'question_text', 'pub_date', 'choices')
